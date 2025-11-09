14026de7f4c36b10ca39f662878716fd1e3914dd
Harsh5840 <shuklaharsh5840@gmail.com>
2025-11-09 17:23:33 +0530

    done

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -134,14 +134,14 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-[30px">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} project
+                {project.title}
               </h2>
               <p className="text-white/60">{project.description}</p>
               <ul className="flex gap-4">
@@ -159,8 +159,8 @@ const Work = () => {
                 <Link href={project.live}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger className="w-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent " />
+                      <TooltipTrigger className="w-[70px] h-[44px] rounded-lg bg-white/5 flex justify-center items-center group hover:bg-accent transition-all">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-primary " />
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Live Demo</p>
@@ -172,8 +172,8 @@ const Work = () => {
                 <Link href={project.github}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger className="w-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent " />
+                      <TooltipTrigger className="w-[70px] h-[44px] rounded-lg bg-white/5 flex justify-center items-center group hover:bg-accent transition-all">
+                        <BsGithub className="text-white text-3xl group-hover:text-primary " />
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Github</p>
@@ -188,13 +188,13 @@ const Work = () => {
             <Swiper
               spaceBetween={30}
               slidesPerView={1}
-              className="xl:h-[520px] mb-12"
+              className="xl:h-[460px] mb-12"
               onSlideChange={handleSlideChange}
             >
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="h-[400px] relative group flex justify-center items-center bg-pink-50/20">
                       <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       <div className="relative w-full h-full">
                         <Image
@@ -208,8 +208,9 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
-              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc50%_-_22px] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-              btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] flex justify-center items-center transition-all"
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[70px] h-[44px] rounded-lg flex justify-center items-center transition-all"
               />
             </Swiper>
           </div>
